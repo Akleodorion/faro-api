514caf1504d9c90e4643b75f5c1dfac90a969ea5
Christian Bondzie <christian.zap@hotmail.fr>
2023-07-21 21:37:09 +0200

    configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,6 +12,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: [:Authorization]
+      expose: [:Authorization] # Uniquement si l'on utilise devise pour la connexion user.
   end
 end
