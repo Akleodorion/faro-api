4c95328bdc2b40d10e4a3e645313a40bf917c3e1
Christian Bondzie <christian.zap@hotmail.fr>
2023-09-07 10:57:40 +0200

    Ajout des validations sur le model Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :user
 
-  validates :name, presence: true # ajouter une longueur minimum
-  validates :description, presence: true # ajouter une longeur minimum
-  validates :date, presence: true # une vérification que la date est entrée est > que la date d'ajourd'hui
+  validates :name, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 50, maximum: 200 }
+  validates :date, presence: true, date: { after_or_equal_to: Time.now }
   validates :location, presence: true # vérifier que la localisation existe bel est bien
-  validates :category, presence: true # vérifier que la category fait parti de la liste des Enum choisis
+  validates :category, inclusion: { in: %w[loisir concert voyage] }
 end
