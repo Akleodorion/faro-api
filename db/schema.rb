915c0cbe967f8d7431018fbb3d1576b5f0a3fdcc
Christian Bondzie <christian.zap@hotmail.fr>
2023-12-06 06:43:17 +0100

    Ajout de la colonne username à member

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_03_073508) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_06_054109) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -81,6 +81,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_03_073508) do
     t.bigint "event_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["event_id"], name: "index_members_on_event_id"
     t.index ["user_id"], name: "index_members_on_user_id"
   end
