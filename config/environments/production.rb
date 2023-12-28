e511fd6bb064313d3ec8441437e01d1a780106d9
Christian Bondzie <christian.zap@hotmail.fr>
2023-12-28 15:05:38 +0000

    add key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,16 @@ Rails.application.configure do
 
 
   config.devise_jwt.secret_key = Rails.application.credentials.devise_jwt_secret_key!
+  config.jwt do |jwt|
+    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
+    jwt.dispatch_requests = [
+      ['POST', %r{^/login$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
+    jwt.expiration_time = 30.minutes.to_i
+  end
 
 
 
