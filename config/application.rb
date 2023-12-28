cd7e3181d5c9ac9277b7851cf7c1ca0642f7d57b
Christian Bondzie <christian.zap@hotmail.fr>
2023-12-28 15:09:37 +0000

    secret key

--- config/application.rb
+++ config/application.rb
@@ -36,15 +36,6 @@ module FaroApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.jwt do |jwt|
-      jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
-      jwt.dispatch_requests = [
-        ['POST', %r{^/login$}]
-      ]
-      jwt.revocation_requests = [
-        ['DELETE', %r{^/logout$}]
-      ]
-      jwt.expiration_time = 30.minutes.to_i
-    end
+
   end
 end
