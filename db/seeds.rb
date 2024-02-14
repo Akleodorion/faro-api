704826f1722a3086290c78a7b7ff780970d3ac40
Christian Bondzie <christian.zap@hotmail.fr>
2024-02-14 13:37:08 +0100

    Modification de la date des seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,44 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-require 'open-uri'
-require 'faker'
-
 
 User.destroy_all
-User.create(email: "test1@gmail.com", password: "1234567", username: "Chris", phone_number: "+2250807090294")
-User.create(email: "test2@gmail.com", password: "1234567", username: "Tressy", phone_number: "+2250807260193")
-User.create(email: "test3@gmail.com", password: "1234567", username: "Aby", phone_number: "+2250807051023")
-User.create(email: "test4@gmail.com", password: "1234567", username: "Elie", phone_number: "+2250807020722")
+User.create(email: 'test1@gmail.com', password: '1234567', username: 'Chris', phone_number: '+2250807090294')
+User.create(email: 'test2@gmail.com', password: '1234567', username: 'Tressy', phone_number: '+2250807260193')
+User.create(email: 'test3@gmail.com', password: '1234567', username: 'Aby', phone_number: '+2250807051023')
+User.create(email: 'test4@gmail.com', password: '1234567', username: 'Elie', phone_number: '+2250807020722')
 
 16.times do
   email = "#{Faker::Name.first_name}.#{Faker::Name.last_name}@gmail.com"
-  password = "#{Faker::Alphanumeric.alphanumeric(number: 10, min_alpha: 3)}"
+  password = Faker::Alphanumeric.alphanumeric(number: 10, min_alpha: 3).to_s
   username = Faker::Ancient.primordial
   phone_number = "+22508#{Faker::PhoneNumber.subscriber_number(length: 8)}"
-  User.create(email: email, password: password, username: username, phone_number: phone_number)
+  User.create(email, password, username, phone_number)
 end
-
 
 users = User.all
 number = 0
 users.each do |user|
-
  3.times do
       random_id = rand(1..400)
       url = "https://api.jikan.moe/v4/manga/#{random_id}"
-      number += 1;
+      number += 1
       begin
         # Call API.
         json_data = URI.open(url).read
         data = JSON.parse(json_data)
 
-
-        #Initialisation des variables
+        # Initialisation des variables
         category = %w[loisir sport concert culture].sample
-        free = [true,false].sample
+        free = [true, false].sample
         description = Faker::Lorem.paragraph(sentence_count: 15, supplemental: false, random_sentences_to_add: 10)
-        date = Faker::Date.between(from: '2023-12-15', to: '2023-12-31')
+        date = Faker::Date.between(from: '2024-2-15', to: '2024-5-31')
 
         country_code = Faker::Address.country_code
         country = Faker::Address.country_by_code(code: country_code)
@@ -62,10 +56,10 @@ users.each do |user|
         platinum_ticket_description = nil
         platinum_ticket_price = nil
         max_platinum_ticket = nil
-        start_time = ['18:00','19:00','20:00','21:00'].sample
-        end_time = ['02:00','03:00','04:00','05:00'].sample
+        start_time = ['18:00', '19:00', '20:00', '21:00'].sample
+        end_time = ['02:00', '03:00', '04:00', '05:00'].sample
 
-        if !free
+        unless free
           standard_ticket_price = rand(1..5) * 1000
           max_gold_ticket = rand(10..15)
           gold_ticket_description = Faker::Lorem.paragraph(sentence_count: 3)
@@ -73,11 +67,7 @@ users.each do |user|
           platinum_ticket_description = Faker::Lorem.paragraph(sentence_count: 3)
           platinum_ticket_price = rand(20..25) * 1000
           max_platinum_ticket = rand(5..10)
-        else
-
         end
-
-
         # upload on cloudinary
         file = URI.open(data['data']['images']['jpg']['large_image_url'])
 
@@ -111,20 +101,20 @@ users.each do |user|
 
 users.each do |user|
   events = Event.where.not(user_id: user.id)
-  userList = User.where.not(id: user.id);
+  user_list = User.where.not(id: user.id);
   3.times do
     event = events.sample
-    type = ["standard", "gold", "platinum"].sample
+    type = %w[standard gold platinum].sample
     if event.free
-      type = "standard"
+      type = 'standard'
     end
-    pickedUser = userList.sample
-    Member.create(event_id: event.id, user_id: pickedUser.id, username:pickedUser.username)
+    picked_user = user_list.sample
+    Member.create(event_id: event.id, user_id: picked_user.id, username: picked_user.username)
     case type
-    when "standard"
-      ticket = Ticket.create(type: type, description: event.standard_ticket_description,
-      price: event.standard_ticket_price, verified: [true,false].sample,
-      user_id: user.id, event_id: event.id)
+    when 'standard'
+      ticket = Ticket.create(type, description: event.standard_ticket_description,
+                                   price: event.standard_ticket_price, verified: [true, false].sample,
+                                   user_id: user.id, event_id: event.id)
 
       # Logique pour générer le QR code (utilisez rqrcode ou une autre bibliothèque)
       qrcode = RQRCode::QRCode.new("#{ticket.id},#{ticket.event_id},#{ticket.type}")
@@ -179,6 +169,5 @@ users.each do |user|
       temp_file.unlink
       ticket.update(qr_code_url: ticket.photo.blob.url)
     end
-
   end
 end
