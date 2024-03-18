10664f38f68d86de056e8e64f8dc5811aa460f41
Christian Bondzie <christian.zap@hotmail.fr>
2024-03-18 17:32:47 +0100

    création des views device

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
-  resources :events, only: %i[create index show update destroy], defaults: { format: 'json' }do
+  resources :events, only: %i[create index show update destroy], defaults: { format: 'json' } do
     member do
       put 'update_activation'
       put 'update_close'
